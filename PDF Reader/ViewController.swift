f8e4997e764b266f12135a0babccc15ec027a081
makson <94912293+Maksymkek@users.noreply.github.com>
2026-02-18 18:02:36 +0200

    Initial Commit

---
+++ PDF Reader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PDF Reader
+//
+//  Created by Максим Грищенков on 18.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PDF ReaderTests/PDF_ReaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_ReaderTests.swift
+//  PDF ReaderTests
+//
+//  Created by Максим Грищенков on 18.02.2026.
+//
+
+import Testing
+@testable import PDF_Reader
+
+struct PDF_ReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
